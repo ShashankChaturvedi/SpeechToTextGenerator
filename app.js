ee25a949b33a0159ebd4a2fef6f55863f9d75849
ShashankChaturvedi <56739878+ShashankChaturvedi@users.noreply.github.com>
2022-01-30 23:49:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const text=document.querySelector(".texts");
+const button=document.querySelector(".button");
+window.SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition=new window.SpeechRecognition();
+recognition.interimResults=true;
+// recognition.continuous=true;
+let p=document.querySelector("p");
+recognition.addEventListener("result",(e)=>{
+    const t=Array.from(e.results)
+    .map(result=>result[0])
+    .map(result=>result.transcript)
+    .join("");
+    p.innerText=t;
+    text.appendChild(p)
+    if(e.results[0].isFinal){
+        p =document.createElement("p")
+    }
+})
+recognition.addEventListener("end",()=>{
+    recognition.start();
+})
+let mode=false;
+button.addEventListener("click",()=>{
+    recognition.start();
+    button.innerText="End"
+})
